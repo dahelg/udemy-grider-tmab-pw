2ee6c1e9e991e2084d4c6393297059c5c2133906
Helge Drews <hdrews87@gmail.com>
2021-01-04 21:29:40 +0100

    feat: React to button click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'udemy-grider-tmab-pw';
+  onButtonClick() {
+    console.log('Button was clicked!');
+  }
 }
